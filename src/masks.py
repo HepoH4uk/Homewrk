13506f5d52f2da7b7c9fd57a1539aa918021f6d2
AlTim <aleks00_777@mail.ru>
2024-12-29 17:06:44 +0300

    add src

---
+++ src/masks.py
@@ -0,0 +1,21 @@
+from typing import Union
+
+
+def get_mask_card_number(card_number: Union[int, str]) -> Union[str]:
+    """Функция маскировки номера банковской карты"""
+
+    str_card_number = str(card_number)
+
+    return f"{str_card_number[:4]} {str_card_number[4:6]}** **** {str_card_number[-4:]}"
+
+
+def get_mask_account(account_number: Union[int, str]) -> Union[str]:
+    """Функция маскировки номера банковского счета"""
+
+    str_account_number = str(account_number)
+
+    return f"**{str_account_number[-5:-1]}"
+
+
+print(get_mask_account(1234567891))
+print(get_mask_card_number(1234567891))
