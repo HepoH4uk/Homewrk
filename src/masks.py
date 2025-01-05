65162a2576cf112042e00795d71978c6df74eb8f
AlTim <aleks00_777@mail.ru>
2025-01-05 15:47:36 +0300

    global fix

--- src/masks.py
+++ src/masks.py
@@ -1,13 +1,10 @@
 from typing import Union
-from widget import mask_account_card
 
 def get_mask_card_number(card_number: Union[int, str]) -> Union[str]:
     """Функция маскировки номера банковской карты"""
 
     str_card_number = str(card_number)
-
     return f"{str_card_number[:4]} {str_card_number[4:6]}** **** {str_card_number[-4:]}"
-
 
 def get_mask_account(account_number: Union[int, str]) -> Union[str]:
     """Функция маскировки номера банковского счета"""
@@ -16,9 +13,4 @@ def get_mask_account(account_number: Union[int, str]) -> Union[str]:
 
     return f"**{str_account_number[-5:-1]}"
 
-card_name, card_num = mask_account_card()
 
-if len(card_num)<17:
-    print(f"{card_name} {get_mask_account(card_num)}")
-else:
-    print(f"{card_name} {get_mask_card_number(card_num)}")
--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,9 @@
-def mask_account_card(account_card = "Maestro 70007922896063657656751"):
+from masks import get_mask_card_number
+from masks import get_mask_account
+
+
+def mask_account_card(account_card):
+    """Функция обработки информации о картах и счетах"""
     card_num = ""
     card_name = ""
     for i in account_card:
@@ -6,6 +11,18 @@ def mask_account_card(account_card = "Maestro 70007922896063657656751"):
             card_num+=i
         else:
             card_name += i
-    return card_name,card_num
 
-print(mask_account_card("Maestro 70007922896063657656751"))
+    if len(card_num)<17:
+        return (f"{card_name} {get_mask_account(card_num)}")
+    else:
+        return (f"{card_name} {get_mask_card_number(card_num)}")
+
+def get_data(user_date: str) -> str:
+    """Функция возвращает дату в корректном формате"""
+    new_date = user_date.split("T")
+    date = new_date[0].split("-")
+
+    return (f"{date[-1]}.{date[-2]}.{date[-3]}")
+
+print(mask_account_card("Maestro 700079228879"))
+print(get_data("2024-03-11T02:26:18.671407"))
