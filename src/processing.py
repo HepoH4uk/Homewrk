f4b1726519e2a90e1aa567ef871d80e37205f205
AlTim <aleks00_777@mail.ru>
2025-01-09 21:27:11 +0300

    small fix processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,11 +1,11 @@
-def filter_by_state(person_list: list, state: str ='EXECUTED') -> list:
+def filter_by_state(person_list: list, state: str = 'EXECUTED') -> list:
     """Функция фильтрующая список словарей по значению state"""
 
     new_person_list = []
 
     for i in person_list:
         if i.get("state") == state:
-           new_person_list.append(i)
+            new_person_list.append(i)
 
     return new_person_list
 
@@ -13,9 +13,4 @@ def filter_by_state(person_list: list, state: str ='EXECUTED') -> list:
 def sort_by_date(person_list: list, reverse: bool = True) -> list:
     """Функция сортирует список словарей по дате"""
 
-    return sorted(person_list, key = lambda x: x["date"], reverse = reverse)
-
-
-print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
-print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
-
+    return sorted(person_list, key=lambda x: x["date"], reverse=reverse)
