4af61dbb6946781a91a053530244eac5044326e6
AlTim <aleks00_777@mail.ru>
2025-01-27 20:43:10 +0300

    update generators.py

--- src/generators.py
+++ src/generators.py
@@ -1,4 +1,23 @@
-def filter_by_currency(transactions, currency):
+from typing import Any, Dict, Iterable, Iterator
+
+
+def filter_by_currency(transactions: Iterable[Dict[str, Any]], currency: str) -> Iterator[Dict[str, Any]]:
+    """Функция возврата транзакций с заданной валютой"""
     for transaction in transactions:
-        if transaction["operation_Amount"]["currency"]["name"] == currency:
+        if transaction["operationAmount"]["currency"]["name"] == currency:
             yield transaction
+
+
+def transaction_descriptions(transactions: Iterable[Dict[str, Any]]) -> Iterator[Dict[str, Any]]:
+    """Функция возврата описания операций"""
+    for transaction in transactions:
+        descriptions = transaction["description"]
+        yield descriptions
+
+
+def card_number_generator(start: int, end: int) -> Iterator[str]:
+    """Функция генерирования номера карты"""
+    for i in range(start, end + 1):
+        card_number = f"{i:016d}"[:4]+" "+f"{i:016d}"[4:8]+" "+f"{i:016d}"[8:12]+" "+f"{i:016d}"[12:16]
+
+        yield card_number
