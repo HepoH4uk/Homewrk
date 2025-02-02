3f684e244c9acff3e6d2428d906f7cc18de65228
AlTim <aleks00_777@mail.ru>
2025-02-02 18:31:30 +0300

    update decorators.py

--- src/decorators.py
+++ src/decorators.py
@@ -1,7 +1,7 @@
 from functools import wraps
 
 
-def log(filename = None):
+def log(filename=None):
     """Декоратор логирования функции, а также ее результаты или возникшие ошибки"""
 
     def decorator(func):
@@ -21,13 +21,16 @@ def log(filename = None):
                         file.write(f"{func.__name__} error: {str(error)}. Inputs: {args}, {kwargs}" + "\n")
                 else:
                     print(f"{func.__name__} error: {str(error)}. Inputs: {args}, {kwargs}")
-                raise error
+
         return wrapper
     return decorator
 
-#Пример использования декоратора
-@log(filename="mylog.txt")
+# Пример использования декоратора
+
+
+@log(filename=None)
 def my_function(x, y):
     return x / y
 
-my_function(1, 2)
+
+my_function(1, "0")
