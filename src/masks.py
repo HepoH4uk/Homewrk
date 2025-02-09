12e98244b0cf0b7320812e312ef2348a92474db8
AlTim <aleks00_777@mail.ru>
2025-02-09 12:29:39 +0300

    flake8 fix masks.py, utils.py

--- src/masks.py
+++ src/masks.py
@@ -1,11 +1,10 @@
 import logging
-from csv import excel
 from typing import Union
 
 
 logger = logging.getLogger('utils')
 logger.setLevel(logging.DEBUG)
-file_handler = logging.FileHandler('../logs/masks.log','w',encoding='utf-8')
+file_handler = logging.FileHandler('../logs/masks.log', 'w', encoding='utf-8')
 file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,7 @@ import logging
 
 logger = logging.getLogger('utils')
 logger.setLevel(logging.DEBUG)
-file_handler = logging.FileHandler('../logs/utils.log','w',encoding='utf-8')
+file_handler = logging.FileHandler('../logs/utils.log', 'w', encoding='utf-8')
 file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
@@ -31,6 +31,3 @@ def list_transactions(filename="../data/operations.json"):
     except json.JSONDecodeError as error:
         logger.error(f"Произошла ошибка: {error}")
         return data
-
-
-print(list_transactions())
